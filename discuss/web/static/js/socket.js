7fbab289cfe07f9ddefce38f0e334f4b149fc37b
sharleenawinja <awinjasharleen@gmail.com>
2026-06-30 06:18:39 +0300

    broadcasting updates

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -65,6 +65,8 @@ const createSocket = (topicId) => {
       console.log("Unable to join", resp);
     });
 
+  channel.on(`comments:${topicId}:new`, renderComment);
+
   document.querySelector("button").addEventListener("click", () => {
     const content = document.querySelector("textarea").value;
 
@@ -74,14 +76,20 @@ const createSocket = (topicId) => {
 
 const renderComments = (comments) => {
   const renderedComments = comments.map((comment) => {
-    return `
-      <li class="collection-item" >
-        ${comment.content}
-      </li>
-    `;
+    return commentTemplate(comment);
   });
 
   document.querySelector(".collection").innerHTML = renderedComments.join("");
 };
+
+const renderComment = (event) => {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector(".collection").innerHTML += renderedComment;
+};
+
+function commentTemplate(comment) {
+  return `<li class="collection-item">${comment.content}</li>`;
+}
 
 window.createSocket = createSocket;
